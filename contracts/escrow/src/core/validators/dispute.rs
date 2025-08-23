1e5bf3d159a4daa86bf86f68ca303c557e5f6fc2
armandocodecr <totovip1464@gmail.com>
2025-08-23 15:11:15 -0600

    fix: remove unused DisputeFeeResult parameter and improve fund validation logic in dispute resolution

--- contracts/escrow/src/core/validators/dispute.rs
+++ contracts/escrow/src/core/validators/dispute.rs
@@ -2,7 +2,6 @@ use soroban_sdk::Address;
 
 use crate::{
     error::ContractError,
-    modules::fee::DisputeFeeResult,
     storage::types::{Escrow, Roles},
 };
 
@@ -13,7 +12,6 @@ pub fn validate_dispute_resolution_conditions(
     approver_funds: i128,
     receiver_funds: i128,
     total_funds: i128,
-    fee_result: &DisputeFeeResult,
     current_balance: i128,
 ) -> Result<(), ContractError> {
     if dispute_resolver != &escrow.roles.dispute_resolver {
@@ -24,20 +22,16 @@ pub fn validate_dispute_resolution_conditions(
         return Err(ContractError::EscrowNotInDispute);
     }
 
-    if approver_funds < 0 || receiver_funds < 0 {
+    if approver_funds <= 0 || receiver_funds <= 0 {
         return Err(ContractError::ApproverOrReceiverFundsLessThanZero);
+    }
+
+    if current_balance < total_funds {
+        return Err(ContractError::InsufficientFundsForResolution);
     }
 
     if total_funds != current_balance {
         return Err(ContractError::ReceiverAndApproverFundsNotEqual);
-    }
-
-    if approver_funds < fee_result.net_approver_funds {
-        return Err(ContractError::InsufficientApproverFundsForCommissions);
-    }
-
-    if receiver_funds < fee_result.net_receiver_funds {
-        return Err(ContractError::InsufficientServiceProviderFundsForCommissions);
     }
 
     Ok(())
