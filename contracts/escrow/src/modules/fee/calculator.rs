5440b4c3f096b6a63d28aa09e13bdbce1ec226e5
armandocodecr <totovip1464@gmail.com>
2025-09-22 17:00:40 -0600

    refactor: remove unused DisputeFeeResult struct and related methods from FeeCalculatorTrait

--- contracts/escrow/src/modules/fee/calculator.rs
+++ contracts/escrow/src/modules/fee/calculator.rs
@@ -16,26 +16,11 @@ pub struct StandardFeeResult {
     pub receiver_amount: i128,
 }
 
-#[derive(Debug, Clone)]
-pub struct DisputeFeeResult {
-    pub wardchain_fee: i128,
-    pub platform_fee: i128,
-    pub net_approver_funds: i128,
-    pub net_receiver_funds: i128,
-}
-
 pub trait FeeCalculatorTrait {
     fn calculate_standard_fees(
         total_amount: i128,
         platform_fee_bps: u32,
     ) -> Result<StandardFeeResult, ContractError>;
-
-    fn calculate_dispute_fees(
-        approver_funds: i128,
-        receiver_funds: i128,
-        platform_fee_bps: u32,
-        total_funds: i128,
-    ) -> Result<DisputeFeeResult, ContractError>;
 }
 
 #[derive(Clone)]
@@ -61,42 +46,6 @@ impl FeeCalculatorTrait for FeeCalculator {
             wardchain_fee,
             platform_fee,
             receiver_amount,
-        })
-    }
-
-    fn calculate_dispute_fees(
-        approver_funds: i128,
-        receiver_funds: i128,
-        platform_fee_bps: u32,
-        total_funds: i128,
-    ) -> Result<DisputeFeeResult, ContractError> {
-        let computed_total = BasicMath::safe_add(approver_funds, receiver_funds)?;
-        if computed_total <= 0 {
-            return Err(ContractError::DivisionError);
-        }
-
-        let wardchain_fee = SafeMath::safe_mul_div(
-            total_funds,
-            WARDCHAIN_FEE_BPS,
-            BASIS_POINTS_DENOMINATOR,
-        )?;
-        let platform_fee =
-            SafeMath::safe_mul_div(total_funds, platform_fee_bps, BASIS_POINTS_DENOMINATOR)?;
-        let total_fees = BasicMath::safe_add(wardchain_fee, platform_fee)?;
-
-        let approver_fee_share =
-            SafeMath::safe_mul_div(approver_funds, total_fees as u32, total_funds)?;
-        let net_approver_funds = BasicMath::safe_sub(approver_funds, approver_fee_share)?;
-
-        let receiver_fee_share =
-            SafeMath::safe_mul_div(receiver_funds, total_fees as u32, total_funds)?;
-        let net_receiver_funds = BasicMath::safe_sub(receiver_funds, receiver_fee_share)?;
-
-        Ok(DisputeFeeResult {
-            wardchain_fee,
-            platform_fee,
-            net_approver_funds,
-            net_receiver_funds,
         })
     }
 }
